6186416c81ab960e50f4d5ac419322911ed92130
zjschulz <zjschulz@gmail.com>
2020-02-18 18:26:03 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+    def display_artist(new_song)
+		if !new_song.artist
+			link_to "Add Artist", edit_song_path(new_song)
+		else
+			link_to new_song.artist.name, artist_path(new_song.artist) 
+	    end
+	end
+
 end
